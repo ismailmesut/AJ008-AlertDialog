1e284b34e436b177fe5cce33ab702ec0eacde1f1
ismailmesut <ismailmesutmujde@gmail.com>
2024-08-24 18:30:19 +0300

    updated: Alert Dialog and Toast Message

--- app/src/main/java/com/ismailmesutmujde/alertdialog/MainActivity.java
+++ app/src/main/java/com/ismailmesutmujde/alertdialog/MainActivity.java
@@ -1,8 +1,11 @@
 package com.ismailmesutmujde.alertdialog;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -13,5 +16,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toast.makeText(MainActivity.this, "Toast Message", Toast.LENGTH_LONG).show();
+
+    }
+
+    public void save(View view) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Save");
+        alert.setMessage("Are you sure?");
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // save
+                Toast.makeText(MainActivity.this, "Saved",Toast.LENGTH_LONG).show();
+            }
+        });
+        alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "Not Saved",Toast.LENGTH_LONG).show();
+            }
+        });
+        alert.show();
     }
 }
